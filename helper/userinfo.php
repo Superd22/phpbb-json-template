5cfbe31edf510d13ea7f98838340c8ba1dcf1cb5
Superd22 <superd001@gmail.com>
2017-12-10 14:28:30 +0100

    Fix(mp) avatar chelous dans les mp

--- helper/userinfo.php
+++ helper/userinfo.php
@@ -6,24 +6,35 @@ class userinfo {
     public $color;
     public $username;
     public $avatar;
-
+    
     function __construct($user_id) {
         $this->user_id = (integer) $user_id;
         $this->hydrate_user();
     }
-
-    private function hydrate_user() {
+    
+    protected function hydrate_user() {
         global $db;
-
+        
         $sql = "SELECT username, user_colour, user_avatar, user_avatar_type FROM " . USERS_TABLE . " WHERE user_id='{$this->user_id}' ";
         $results = $db->sql_query($sql);
-
+        
         $r = $db->sql_fetchrow($results);
-
+        
         $this->color = $r["user_colour"];
         $this->username = $r["username"];
-
-        // TO DO PROPER AVATAR
+        
         $this->avatar = $r["user_avatar"];
+        
+        $this->doAvatar();
     }
+    
+    public function doAvatar($avatar = "") {
+        if($this->avatar && strpos($this->avatar, "http") === 0) return;
+        elseif ($this->avatar) {
+            $this->avatar = "./download/file.php?avatar=".$this->avatar;
+        }
+
+        return $this->avatar;
+    }
+
 }
